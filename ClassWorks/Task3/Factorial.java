cd96071bcc0f0f0b5100086375630418b490e514
ReginaRR <Regiregi6@mail.ru>
2017-03-11 18:51:09 +0300

    add classworks

--- ClassWorks/Task3/Factorial.java
+++ ClassWorks/Task3/Factorial.java
@@ -5,6 +5,14 @@ class Factorial {
 		else {
 			return fact(n-1) * n;
 		}
+		public static int fib(int n) {
+			if (n > 1) {
+				return fib(n - 1) + fib(n - 2);
+			
+			} else {
+				return 1;
+			}
+		}
 	}
 	public static void main(String[] args) {
 		System.out.println(fact(5));
