295e2ef2cb09d88b2224e28346699fdf324f299d
ReginaRR <Regiregi6@mail.ru>
2017-03-14 23:26:30 +0300

    add homeworks

---
+++ HomeWorks/Task9/Program.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Program {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		int a[][] = new int[n][m];
+		int num = 0;
+		
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				a[i][j] = scanner.nextInt();
+
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			int sum = 0;
+			num++;
+
+			for (int j = 0; j < n; j++) {
+				sum += a[i][j];
+			}
+			System.out.println("Sum of "+ num + " line = "+ sum);
+		}
+	}
+}
