bbd4006194af4f48bf62ac63866dbbf0d7b0ae68
ReginaRR <Regiregi6@mail.ru>
2017-04-08 17:22:14 +0300

    Add files via upload

---
+++ ClassWorks/Task/src/Fig.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Student8 on 08.04.2017.
+ */
+interface Fig {
+    double area = 1.0;
+    double perimetr = 1.0;
+
+}
---
+++ ClassWorks/Task/src/Triangle.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Student8 on 08.04.2017.
+ */
+
+    public class Triangle implements Fig  {
+        Triangle instance = new Triangle();
+    }
+
