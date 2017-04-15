6cc0421e3e75bd7d31d4d313698b075a697ea169
Student8 <Regiregi6@mail.ru>
2017-04-15 17:33:47 +0300

    R

---
+++ ClassWorks/Task10/src/MainOutputStream.java
@@ -0,0 +1,12 @@
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class MainOutputStream {
+    public static void main() throws Exception {
+        OutputStream outputStream =
+                new FileOutputStream("output.txt");
+    }
+
+
+}
+
