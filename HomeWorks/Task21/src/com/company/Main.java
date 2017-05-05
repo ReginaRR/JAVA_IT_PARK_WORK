4cc6834665fddd02d77ffeaae50fd5446b0d3d5a
ReginaRR <Regiregi6@mail.ru>
2017-05-06 02:14:32 +0300

    Add files via upload

--- HomeWorks/Task21/src/com/company/Main.java
+++ HomeWorks/Task21/src/com/company/Main.java
@@ -1,25 +1,15 @@
 package com.company;
-
 import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = 0;
-        int l = 1;
-        char a[] = new char[n];
-        for(int i = 0; i < n; i++) {
-            a[i] = (char)scanner.nextInt();
+        String l = scanner.nextLine();
+        char a[] = l.toCharArray();
+
+        int num = 0;
+        for (int i = 0; i < a.length; i++) {
+            num = num * 10 + (a[i] - '0');
         }
-
-        for(int i = n - 1; i >= 0; i--) {
-            k += a[i] * l;
-            l *= 10;
-        }
-        System.out.println(k);
-
-
-
+        System.out.println(num);
     }
 }
